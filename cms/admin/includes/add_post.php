1832f528be00c9285a73a812e68062ba85507591
Henrique Dias <henriquediasjr@gmail.com>
2022-07-18 16:39:18 -0300

    today progress

--- cms/admin/includes/add_post.php
+++ cms/admin/includes/add_post.php
@@ -11,7 +11,7 @@ if(isset($_POST['create_post'])){
     $post_image_temp = $_FILES['image']['tmp_name'];
 
     $post_tags = $_POST['post_tags'];
-    $post_content = $_POST['post_content'];
+    $post_content = mysqli_escape_string($connection, $_POST['post_content']);
     $post_date = date('d-m-y');
 
     move_uploaded_file($post_image_temp, "../images/$post_image");
@@ -23,6 +23,10 @@ if(isset($_POST['create_post'])){
     $create_post_query = mysqli_query($connection, $query);
 
     confirmQuery($create_post_query);
+
+    $the_post_id = mysqli_insert_id($connection);
+
+    echo "<p class='bg-success'>Post Created! <a href='../post.php?p_id={$the_post_id}'>View Post</a> or <a href='posts.php'>Edit More Posts</a></p>";
 }
 
 
@@ -67,8 +71,15 @@ if(isset($_POST['create_post'])){
 
 
     <div class="form-group">
-        <label for="post_status">Post Status</label>
-        <input type="text" class="form-control" name="post_status">
+
+        <select name="post_status" id="">
+            
+            <option value="draft">Post status</option>
+            <option value="published">Published</option>
+            <option value="draft">Draft</option>
+
+        </select>
+
     </div>
     
     <div class="form-group">
--- cms/admin/includes/view_all_posts.php
+++ cms/admin/includes/view_all_posts.php
@@ -3,10 +3,42 @@
 
     if(isset($_POST['checkBoxArray'])) {
 
-        foreach($_POST['checkBoxArray'] as $checkBoxValue){
+        foreach($_POST['checkBoxArray'] as $postValueId){
 
             $bulk_options = $_POST['bulk_options'];
 
+            switch($bulk_options) {
+                case 'published':
+
+                    $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postValueId} ";
+
+                    $update_to_published_status = mysqli_query($connection, $query); 
+
+                    confirmQuery($update_to_published_status);
+
+                break;
+
+                case 'draft':
+
+                    $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postValueId} ";
+
+                    $update_to_draft_status = mysqli_query($connection, $query); 
+
+                    confirmQuery($update_to_draft_status);
+
+                break;
+
+                case 'delete':
+
+                    $query = "DELETE FROM posts WHERE post_id = {$postValueId} ";
+
+                    $update_to_delete_status = mysqli_query($connection, $query); 
+
+                    confirmQuery($update_to_delete_status);
+
+                break;
+
+            }
         }
 
     }
@@ -20,14 +52,13 @@
 
     <table class="table table-bordered table-hover">
 
-        <div id="bulkOptionsContainer" class="col-xs-4">
+        <div id="bulkOptionContainer" class="col-xs-4">
 
             <select class="form-control" name="bulk_options" id="">
-
                 <option value="">Select Options</option>
-                <option value="">Publish</option>
-                <option value="">Draft</option>
-                <option value="">Delete</option>
+                <option value="published">Publish</option>
+                <option value="draft">Draft</option>
+                <option value="delete">Delete</option>
                 
 
             </select>
@@ -37,7 +68,7 @@
         <div class="class-xs-4">
 
             <input type="submit" name="submit" class="btn btn-success" value="apply">
-            <a href="add_post.php" class="btn btn-primary">Add New</a>
+            <a href="posts.php?source=add_post" class="btn btn-primary">Add New</a>
 
         </div>
 
@@ -81,7 +112,7 @@
 
                     ?>
 
-                    <td><input class='checkBoxes ' type='checkbox' name='checkBoxArray[]' value='<?php echo $post_id; ?>'></td>
+                    <td><input class='checkBoxes' type='checkbox' name='checkBoxArray[]' value='<?php echo $post_id; ?>'></td>
 
                     <?php
 
